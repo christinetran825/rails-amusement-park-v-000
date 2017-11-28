876c1e0dc6eb30b8dfd2f549ec9fefe1d54880b0
Christine Tran <christinetran825@gmail.com>
2017-11-28 14:14:58 -0800

    added user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UserController < ApplicationController
+
+  before_action :require_login, only: [:show]
+
+  def show
+    @user = User.find(id: params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :happiness, :nausea, :tickets, :height, :admin)
+  end
+
+end
