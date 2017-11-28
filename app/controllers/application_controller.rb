22bee98dce5200a9a2f463fb6ca657a5087d69bf
Christine Tran <christinetran825@gmail.com>
2017-11-28 14:05:28 -0800

    updated current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :curent_user, :logged_in?
 
   def logged_in?
     !!current_user
@@ -14,9 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    User.find_by(id: session[:user_id])
   end
-
-  
 
 end
